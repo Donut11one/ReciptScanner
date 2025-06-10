2f3d4e4a0466578979a5a83b4e9ed78ee2a0750f
Alex He <donut11one@gmail.com>
2025-06-10 12:47:27 -0700

    added tailwindcss to project

--- src/component/TopBar/TopBar.tsx
+++ src/component/TopBar/TopBar.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './TopBar.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./TopBar.css";
 
 const TopBar: React.FC = () => {
-    return (
-        <nav className="topbar">
-            <Link to="/">Home</Link>
-            <Link to="/translate">Translate</Link>
-        </nav>
-    );
+  return (
+    <ul className="flex p-3">
+      <li className="-mb-px mr-1">
+        <Link
+          to="/"
+          className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-black-700 font-semibold"
+        >
+          Home
+        </Link>
+      </li>
+      <li className="mr-1">
+        <Link
+          to="/translate"
+          className="bg-white inline-block py-2 px-4 text-black-500 hover:text-blue-800 font-semibold"
+        >
+          Translate
+        </Link>
+      </li>
+    </ul>
+  );
 };
 
 export default TopBar;
