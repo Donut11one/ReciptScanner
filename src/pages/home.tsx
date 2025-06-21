dfe7554a091db8b1165a48510c2550dd1c491192
Alex He <donut11one@gmail.com>
2025-06-20 19:22:13 -0700

    Revert "BodyStory Section added Still need to figure out Images"
    This reverts commit 0881ec79b74e90bab67fd734ae15b7626e04cc4e.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import BodyStory from "../component/HomePage/BodyStory/BodyStory"
-// import ImagePlaceholder from "../assets/PlaceHolder.jpg";
 
 const Home: React.FC = () => {
   return (
@@ -9,22 +7,7 @@ const Home: React.FC = () => {
         <h1>hero section placeholder</h1>
       </div>
       <div className="body-side-story-section">
-      <BodyStory
-        title="Recipt Scan"
-        imageSrc = {""}
-        imagePosition="right"
-        content={`
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        `}
-      />
-      <BodyStory
-        title="Recipt Scan"
-        imageSrc = {""}
-        imagePosition="left"
-        content={`
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        `}
-      />
+        <h1>body story placeholder</h1>
       </div>
       <div className="body-scrollable-image-section">
         <h1>body scroll image placeholder</h1>
