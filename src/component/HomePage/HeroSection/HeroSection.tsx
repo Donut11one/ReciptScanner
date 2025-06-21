3862887782d4a7f93c02ab5ed97af36d6b98b1dd
Alex He <donut11one@gmail.com>
2025-06-21 15:31:57 -0700

    Added image loading and styled components

--- src/component/HomePage/HeroSection/HeroSection.tsx
+++ src/component/HomePage/HeroSection/HeroSection.tsx
@@ -1,20 +1,41 @@
 import React from "react";
-import "./HeroSection.scss";
 import { Link } from "react-router-dom";
+import Styled from "styled-components";
+import heroBackground from "../../../assets/HeroBackground.jpg";
+
+const HeroComponent = Styled.div`
+      background-image: url(${heroBackground});
+      background-size: cover;
+      height: 600px;
+      `;
+
+const HeroContentComponent = Styled.div`
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      height: 100%;
+      color: white;
+`;
+
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="p-5 text-center bg-image">
-      <div className="d-flex justify-content-center align-items-center h-100">
-        <div className="text-white w-50">
-          <h1 className="mb-3 ">Recipt Scanner</h1>
-          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dolore iusto incidunt debitis exercitationem consectetur nostrum error cupiditate quam inventore tempore rem nesciunt nisi ex, ab fugit sit minima possimus.</p>
-          <Link to="/translate" className="btn btn-outline-light btn-lg">
-            Upload Now
-          </Link>
-        </div>
-      </div>
-    </div>
+    <HeroComponent>
+      <HeroContentComponent>
+        <h1 className="mb-3 ">Recipt Scanner</h1>
+        <p style={{width: "800px"}}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga dolore
+          iusto incidunt debitis exercitationem consectetur nostrum error
+          cupiditate quam inventore tempore rem nesciunt nisi ex, ab fugit sit
+          minima possimus.
+        </p>
+        <Link to="/translate" className="btn btn-outline-light btn-lg">
+          Upload Now
+        </Link>
+      </HeroContentComponent>
+    </HeroComponent>
   );
 };
 export default HeroSection;
