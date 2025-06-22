c913fdb082e801ecbde08703370174f0e1c9514a
Alex He <donut11one@gmail.com>
2025-06-21 17:59:04 -0700

    Body story (#8)
    * added bodystory problem with images
    
    * Added images and switched to using styled component

---
+++ src/component/HomePage/BodyStory/BodyStory.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+const BodyStoryComponent = styled.div`
+  background-color: black;
+  color: white;
+  padding: 32px 80px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+`;
+
+const ImageContainer = styled.div`
+  img {
+    width: 350px;
+    height: 350px;
+  }
+`;
+
+const TextContainer = styled.div`
+  h4 {
+    font-weight: bold;
+    text-transform: uppercase;
+    margin: 0 0 16px 0;
+  }
+`;
+
+const BodyStory = (props: any) => {
+  const { title, content, imageSrc, isImageRight } = props;
+  return (
+    <BodyStoryComponent>
+      {!isImageRight && (
+        <ImageContainer>
+          <img src={imageSrc} alt={title} />
+        </ImageContainer>
+      )}
+      <TextContainer>
+        <h4>{title}</h4>
+        <p>{content}</p>
+      </TextContainer>
+      {isImageRight && (
+        <ImageContainer>
+          <img src={imageSrc} alt={title} />
+        </ImageContainer>
+      )}
+    </BodyStoryComponent>
+  );
+};
+
+export default BodyStory;
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import HeroSection from "../component/HomePage/HeroSection/HeroSection"
+import BodyStory from "../component/HomePage/BodyStory/BodyStory";
+import CatTyping from "../assets/CatTyping.gif";
+import HandScanning from "../assets/Scanning.gif"
 
 const HomePage: React.FC = () => {
   return (
@@ -8,7 +11,21 @@ const HomePage: React.FC = () => {
         <HeroSection/>
       </div>
       <div className="body-side-story-section">
-        <h1>body story placeholder</h1>
+      <BodyStory
+        title="Recipt Scan"
+        imageSrc = {CatTyping}
+        content={`
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        `}
+      />
+      <BodyStory
+        title="Recipt Scan"
+        imageSrc = {HandScanning}
+        isImageRight
+        content={`
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        `}
+      />
       </div>
       <div className="body-scrollable-image-section">
         <h1>body scroll image placeholder</h1>
